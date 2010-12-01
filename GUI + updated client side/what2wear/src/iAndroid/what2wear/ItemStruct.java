054846654e4317598e5a2c192add266b15fb884f
cohenhila4 <cohenhila4@674b6961-f19a-2df0-4254-48ddb6ca9438>
2010-12-01 21:31:41 +0000

    The GUI and client side files for milestone 1.

---
+++ GUI + updated client side/what2wear/src/iAndroid/what2wear/ItemStruct.java
@@ -0,0 +1,12 @@
+package iAndroid.what2wear;
+
+public class ItemStruct {
+	public String item_type;
+	public String item_color;
+
+	public ItemStruct(){
+		item_type = "";
+		item_color = "";
+	}
+
+}
